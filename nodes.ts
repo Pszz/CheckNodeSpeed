07716ddcff8e28862bac0d468030372a33281047
Pipi <pi951357@gmail.com>
2023-07-31 17:51:50 +0800

    Create nodes.ts

---
+++ nodes.ts
@@ -0,0 +1,10 @@
+import { NodeType } from './CheckNodeSpeed'
+
+export const Nodes: NodeType[] = [
+  // default {prot: 443, protocol: 'https'}
+  {
+    host: 'mainnet.suiet.app',
+  },
+  { host: 'rpc.ankr.com/sui/1a32bff2f1b8b2f4c9b49f5fa13c5eee216c3c9018b61c2e8757900d35fcbfc8' },
+  { host: 'rpc.mainnet.sui.io' },
+]
